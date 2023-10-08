e471239f211da8043498d310dead78d38e39665c
JiaoZ769 <38471105+jiaoZ7688@users.noreply.github.com>
2023-10-08 17:29:56 +0800

    Update demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -195,7 +195,7 @@ def detect(cfg,opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='/media/zhanjiao/workspace/YOLOPv3/weights/epoch-189.pth', help='model.pth path(s)')
+    parser.add_argument('--weights', type=str, default='/media/zhanjiao/workspace/YOLOPv3/weights/epoch-189.pth', help='model.pth path(s)')
     parser.add_argument('--source', type=str, default='/media/zhanjiao/workspace/YOLOPv3/inference/image', help='source')  # file/folder   ex:inference/images
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
